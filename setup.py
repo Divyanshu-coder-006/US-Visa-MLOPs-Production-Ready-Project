1a35f33af93a6300574d49d98190d7e6530d17c5
Divyanshu Verma <divyanshuv871@gmail.com>
2026-01-16 19:07:53 +0530

    mongodb demo added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Divyanshu_Verma",
+    author_email="divyanshuverma020@gmail.com",
+    packages= find_packages()
+)
